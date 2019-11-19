56394a0a41f9e69c0471c5fff41c362facdda948
nikolaLazarevic95 <nikola.lazaa@gmail.com>
2019-11-19 19:43:57 +0100

    Init commit

---
+++ src/utils/users.js
@@ -0,0 +1,59 @@
+const users = []
+
+// addUser, removeUser, getUser, getUsersInRoom
+
+const addUser = ({ id, username, room }) => {
+    // Clean the data
+    username = username.trim().toLowerCase()
+    room = room.trim().toLowerCase()
+
+    // Validate the data
+    if (!username || !room) {
+        return {
+            error: 'Username and room are required!'
+        }
+    }
+
+    // Check for existing user
+    const existingUser = users.find((user) => {
+        return user.room === room && user.username === username
+    })
+
+    // Validate username
+    if (existingUser) {
+        return {
+            error: 'Username is in use!'
+        }
+    }
+
+    // Store user
+    const user = { id, username, room }
+    users.push(user)
+    return { user }
+}
+
+const removeUser = (id) => {
+    const index = users.findIndex((user) => user.id === id)
+
+    if (index !== -1) {
+        return users.splice(index, 1)[0] // splice da delete
+        //!The .splice() method returns an array of elements that were deleted from the original
+        //! array so the [0] afterward is just accessing the first element of the returned elements.
+    }
+}
+
+const getUser = (id) => {
+    return users.find((user) => user.id === id)
+}
+
+const getUsersInRoom = (room) => {
+    return users.filter((user) => user.room === room)
+}
+
+
+module.exports = {
+    addUser,
+    removeUser, 
+    getUser, 
+    getUsersInRoom
+}
